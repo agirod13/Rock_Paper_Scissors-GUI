2c7053508b08b2fe936405e8e42fef5c972cf6c6
agirod13 <agirod13@gmail.com>
2022-08-06 22:30:57 -0400

    Finished basic build

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
-const choices = ["rock", "paper", "scissors"]
+const choices = ["rock", "paper", "scissors"];
+const winners = [];
 
 function game() {
-    playRound();
+    for (let i = 1; i <= 5; i++) {
+        playRound(i);
+    }
+    logWins();
 };
 
-function playRound() {
+function playRound(round) {
     const playerSelection = playerChoice();
     const computerSelection = computerChoice();
+    const winner = checkWinner(playerSelection, computerSelection);
+    winners.push(winner);
+    logRound(playerSelection, computerSelection, winner, round);
 };
 
 function playerChoice() {
     let input = prompt('Type Rock, Paper, or Scissors');
-    while(input == null){
+    while(input == null) {
         input = prompt('Type Rock, Paper, or Scissors');
     }
     input = input.toLowerCase();
-    let check = validateInput(input)
+    let check = validateInput(input);
     while (check == false) {
         input = prompt('Type Rock, Paper, or Scissors');
+        while(input == null) {
+            input = prompt('Type Rock, Paper, or Scissors');
+        }
+        input = input.toLowerCase();
+        check = validateInput(input);
     }
-    while(input == null){
-        input = prompt('Type Rock, Paper, or Scissors');
-    }
-    input = input.toLowerCase();
-    check = validateInput(input);
+    return input;
 };
-
 
 function computerChoice() {
     return choices[Math.floor(Math.random()*choices.length)]
@@ -35,4 +42,36 @@ function validateInput(choice) {
     return choices.includes(choice)
 };
 
-game();
+function checkWinner(choiceP, choiceC){
+    if(choiceP === choiceC) {
+        return "Tie";
+    } else if (
+        (choiceP === "rock" && choiceC === "scissors") || 
+        (choiceP === "paper" && choiceC === "rock") ||
+        (choiceP === "scissors" && choiceC === "paper")
+    ) {
+        return "Player";
+    } else {
+        return "Computer";
+    }
+};
+
+function logWins(){
+    let playerWins = winners.filter((item) => item == "Player").length;
+    let computerWins = winners.filter((item) => item == "Computer").length;
+    let ties = winners.filter((item) => item == "Tie").length;
+    console.log("Results:");
+    console.log("Player wins:", playerWins);
+    console.log("Computer wins:", computerWins);
+    console.log("Ties", ties);
+
+};
+
+function logRound(playerChoice, computerChoice, winner, round){
+    console.log("Round", round);
+    console.log("Player Chose:", playerChoice);
+    console.log("Computer Chose:", computerChoice);
+    console.log(winner, "Won the Round");
+    console.log("-----------------------------------------");
+};
+
